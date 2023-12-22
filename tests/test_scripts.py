9bc93a955e865569efcc39b9979aac13c431ea4f
wangqs <wqs1639@gmail.com>
2023-12-22 11:11:43 +0800

    format code

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -8,4 +8,3 @@ def test_scripts_entrypoint():
         p = subprocess.run(f"{executable} --version", shell=True)
         if p.returncode != 0:
             sys.exit(1)
-        # p = subprocess.run(f"{executable} -v", shell=True, capture_output=True)
